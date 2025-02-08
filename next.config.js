3197374e5ae8f0cbacb59b24e54f967c3a6524e7
Shivoo29 <2004skj@gmail.com>
2025-02-09 02:41:59 +0530

    pushing my limits

--- next.config.js
+++ next.config.js
@@ -26,6 +26,24 @@ const nextConfig = {
 			},
 		]
 	},
+	// Add assetPrefix for production
+	assetPrefix: process.env.NODE_ENV === 'production' 
+		? 'https://zennith-esummit.vercel.app' 
+		: '',
+	// Configure webpack to handle GLB files
+	webpack: (config) => {
+		config.module.rules.push({
+			test: /\.(glb|gltf)$/,
+			use: {
+				loader: 'file-loader',
+				options: {
+					publicPath: '/_next/static/models',
+					outputPath: 'static/models',
+				},
+			},
+		});
+		return config;
+	},
 }
 
 module.exports = nextConfig
